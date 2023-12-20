447f97f25f48fb984678f751467b338ceb4f9496
kassim-gif <shuaibkassim44@gmail.com>
2023-12-19 23:49:51 -0800

    update 2

--- _atoi.c
+++ _atoi.c
@@ -1,169 +1,74 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is chain delimiter
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address for current pstn in buf
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 1 if  chain delimiter, 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int is_chain(info_t *info, char *buf, size_t *p)
+int interactive(info_t *info)
 {
-    size_t j = *p;
-
-    if (buf[j] == '|' && buf[j + 1] == '|')
-    {
-        buf[j] = 0;
-        j++;
-        info->cmd_buf_type = CMD_OR;
-    }
-    else if (buf[j] == '&' && buf[j + 1] == '&')
-    {
-        buf[j] = 0;
-        j++;
-        info->cmd_buf_type = CMD_AND;
-    }
-    else if (buf[j] == ';') /* found end of this command */
-    {
-        buf[j] = 0; /* replace semicolon with null */
-        info->cmd_buf_type = CMD_CHAIN;
-    }
-    else
-        return (0);
-    *p = j;
-    return (1);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * check_chain - check if couldd continue chaining on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
- *
- * Return: Void
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
-void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+int is_delim(char c, char *delim)
 {
-    size_t j = *p;
-
-    if (info->cmd_buf_type == CMD_AND)
-    {
-        if (info->status)
-        {
-            buf[i] = 0;
-            j = len;
-        }
-    }
-    if (info->cmd_buf_type == CMD_OR)
-    {
-        if (!info->status)
-        {
-            buf[i] = 0;
-            j = len;
-}
-    }
-
-    *p = j;
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
- * replace_alias - replaces an alias in tha tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, else 0 is returned
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
-int replace_alias(info_t *info)
+
+int _isalpha(int c)
 {
-    int i;
-    list_t *node;
-    char *p;
-
-    for (i = 0; i < 10; i++)
-    {
-        node = node_starts_with(info->alias, info->argv[0], '=');
-        if (!node)
-            return (0);
-        free(info->argv[0]);
-        p = _strchr(node->str, '=');
-        if (!p)
-            return (0);
-        p = _strdup(p + 1);
-        if (!p)
-            return (0);
-        info->argv[0] = p;
-    }
-    return (1);
-}
-/**
- * replace_vars - replacs vars in tha tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
-int replace_vars(info_t *info)
-{
-    int i = 0;
-    list_t *node;
-
-    for (i = 0; info->argv[i]; i++)
-    {
-        if (info->argv[i][0] != '$' || !info->argv[i][1])
-            continue;
-
-        if (!_strcmp(info->argv[i], "$?"))
-        {
-            replace_string(&(info->argv[i]),
-                            _strdup(convert_number(info->status, 10, 0)));
-            continue;
-        }
-        if (!_strcmp(info->argv[i], "$$"))
-        {
-            replace_string(&(info->argv[i]),
-                            _strdup(convert_number(getpid(), 10, 0)));
-            continue;
-        }
-        node = node_starts_with(info->env, &info->argv[i][1], '=');
-        if (node)
-        {
-            replace_string(&(info->argv[i]),
-                            _strdup(_strchr(node->str, '=') + 1));
- continue;
-        }
-        replace_string(&info->argv[i], _strdup(""));
-    }
-    return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * replace_string - replaces string
- * @old: address of tha old string
- * @new: new string
- *
- * Return: 1 if replaced else 0
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
-int replace_string(char **old, char *new)
-{
-    free(*old);
-    *old = new;
-    return (1);
-}
 
-/**
- * is_Character_Delimiter - checks if char is delimiters
- * @character: the char to check
- * @delimiter_string: tha string containing delimiters
- * Return: 1 if the character is dm, otherwise 0 is returned
- */
-int is_Character_Delimiter(char character, char *delimiter_string)
+int _atoi(char *s)
 {
-    while (*delimiter_string)
-    {
-        if (*delimiter_string == character)
-            return 1;
-        delimiter_string++;
-}
-    return 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
